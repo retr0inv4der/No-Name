14f8a7e188a36e8d37b2e00f9ae65f9d70f3c2a1
retr0inv4der <retr0.inv4der@gmail.com>
2026-06-06 07:40:41 +0330

    improving speech recognition

--- soldier_voice.py
+++ soldier_voice.py
@@ -1,52 +1,40 @@
 from urllib.error import URLError
 from urllib.request import urlopen
 import speech_recognition as sr
-import pyttsx3
 from gtts import gTTS
-from pygame import mixer
-import random
 import aiml
 import os
-import time
-import sys
 import subprocess
+import whisper
+import numpy as np
+import tempfile
+
+
+
 
 class bot : 
     def __init__(self):
-        self.internet = None 
-  
+        self.model = whisper.load_model("base")
 
-    def ping(self):
-        
-        try:
-            urlopen('http://google.com', timeout=1)
-            self.internet = "online"
-            return True
-        except URLError as err:
-            self.internet = "offline"
-            return False
 
 
     def listen(self):
-        
         r = sr.Recognizer()
         with sr.Microphone() as source:
             print("I am listening: ")
             audio = r.listen(source)
-        try:
-            if self.internet == "online":
-                data = r.recognize_google(audio) 
-                print(data)
-                return  data
-            else:
-                data = r.recognize_sphinx(audio)
-                print(data)
-                return  data
-        except sr.UnknownValueError:
-            self.speak("I couldn't understand what you said! Would you like to repeat?")
-            return(self.listen())
-        except sr.RequestError as e:
-            print("Could not request results from speech service; {0}".format(e))
+        
+        # Save to a temporary WAV file
+        with tempfile.NamedTemporaryFile(suffix=".wav", delete=False) as temp_file:
+            temp_file.write(audio.get_wav_data())
+            temp_path = temp_file.name
+        
+        # Load with Whisper
+        data = self.model.transcribe(temp_path)
+        
+
+        print(data["text"])
+        return data["text"]
 
     def speak(self , text ,):
         if self.internet == "online":
@@ -63,7 +51,9 @@ class bot :
 
 
     def mainloop(self):
-        self.ping()
+        #init
+        
+
         kernel = aiml.Kernel()
         if os.path.isfile("bot_brain.brn"):
             kernel.bootstrap(brainFile = "bot_brain.brn")
